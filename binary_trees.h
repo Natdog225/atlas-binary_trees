3889f86067422ffe18272862083927a9b5c1f54a
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-11 14:37:25 -0500

    Removed helper from .h

--- binary_trees.h
+++ binary_trees.h
@@ -41,7 +41,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
-static int _is_perfect_recursive(const binary_tree_t *tree, size_t depth, size_t level);
 
 
 
