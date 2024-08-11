b661c42d66e3c5de608ccc023b62fb98614a697e
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-10 21:01:51 -0500

    Update

--- binary_trees.h
+++ binary_trees.h
@@ -25,9 +25,8 @@ typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-// ... other function prototypes
 
-/* Helper functions for binary_tree_print can be declared here */
+
 static int print_t(const binary_tree_t *tree, int offset, int depth,
 				   char **s);
 static size_t _height(const binary_tree_t *tree);
