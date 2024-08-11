f230303efcc731506510c750ed86397894ee8a62
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-11 13:29:13 -0500

    Added betty comments

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,11 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height   
+
+ *
+ * Return: The height of the tree. 0 if tree is NULL
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
