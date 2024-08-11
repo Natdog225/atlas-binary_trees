b2c6c97b212f1f0d863291307d514bbe025bed01
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-11 14:53:49 -0500

    Betty fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->left == node)
-		return (node->parent->right); /* Node is left child, sibling is right child */
+		return (node->parent->right); /* Node is left child, sibling right child */
 	else
 		return (node->parent->left); /* Node is right child, sibling is left child */
 }
