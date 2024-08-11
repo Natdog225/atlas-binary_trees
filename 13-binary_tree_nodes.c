6bbda1ae017f8409dcd6281a12bd0e30a6eb4652
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-11 15:03:27 -0500

    Betty fixes.

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,7 +4,7 @@
  * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
  * @tree: Pointer to the root node of the tree to count the number of nodes
  *
- * Return: The number of nodes with at least 1 child in the tree. 0 if tree is NULL
+ * Return: The number of nodes with at least 1 child in the tree. 0 if NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
@@ -14,6 +14,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0); /* This is a leaf node, not counted */
 
-	/* Recursively count nodes in left and right subtrees, add 1 for current node */
+	/* Recursively counts in left and right subtrees, add 1 for current node */
 	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
